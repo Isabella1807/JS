ea4feb5cf899a605e571d19f56e5c9837f923db5
Isabella1807 <megaoreomuffin@hotmail.com>
2023-01-31 10:22:44 +0100

    sum js

--- gang2/gang2_øvelser.js
+++ gang2/gang2_øvelser.js
@@ -2,4 +2,11 @@ alert("I ekstern");
 
 console.log("Isabella");
 
-document.body.innerHTML = "<h1>Mit nye website</h1>";
+// document.body.innerHTML = "<h1>Mit nye website</h1>";
+
+const hunderace = "Dalmatiner";
+let hundealder = 20;
+
+const hunden = hunderace + " er " + hundealder + " år gammel.";
+
+document.getElementById("Hunden").innerText = hunden;
