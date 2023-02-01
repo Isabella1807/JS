ba15450438b5a103fb33bfdc1fd2c0df62a7b9c0
Isabella1807 <megaoreomuffin@hotmail.com>
2023-02-01 11:23:37 +0100

    øvelser

--- gang3_operator_arrays_booleans_if_else/gang3_øvelser.js
+++ gang3_operator_arrays_booleans_if_else/gang3_øvelser.js
@@ -1,22 +1,51 @@
 console.log("JS virker");
 
-let animals = ["panda", "lion","gorilla"];
+// let animals = ["panda", "lion","gorilla"];
+//
+// if (animals[1]=="gorilla"){
+//     document.getElementById("Zoo").innerText="Der er en gorilla";
+//     alert("en gorilla");
+// } else {
+//     document.getElementById("Zoo").innerText="Der er ingen gorilla her";
+//     alert("Ingen gorilla");
+// }
+//
+// document.getElementById("liste").innerHTML+="liste af dyr"
+// for (var i = 0; i < animals.length; i++) {
+//     document.getElementById("liste").innerHTML+= "<br>" + animals[i];
+// }
+// //i = 0 - lav en variable og giv den værdien 0
+// // i < animals.length - hvis i er mindre end længden af arreyet animals
+// // i++ - hvis i er mindre end animals, gør i større
+// //og den vil køre document.getelementbyid linje hvergang i ikke er større end animals
+// // += tilføjer istedet for at erstatte
+//
+//
+// function MyFunctionName(myFunctionParameter1, MyFunctionParameter2) {
+//
+// }
 
-if (animals[1]=="gorilla"){
-    document.getElementById("Zoo").innerText="Der er en gorilla";
-    alert("en gorilla");
-} else {
-    document.getElementById("Zoo").innerText="Der er ingen gorilla her";
-    alert("Ingen gorilla");
+//Just adding together
+function myFunction (number, number){
+    return number * number;
+}
+document.getElementById("functionPlace").innerText = myFunction(5,4);
+
+// const Result = myFunction(5, 2){
+//     document.getElementById("functionPlace").innerText = Result;
+// }
+
+let farver = ["red", "green","blue","yellow"];
+document.getElementById("knappen").style.color = RandomColor;
+
+function RandomColor(){
+
 }
 
-document.getElementById("liste").innerHTML+="liste af dyr"
-for (var i = 0; i < animals.length; i++) {
-    document.getElementById("liste").innerHTML+= "<br>" + animals[i];
-}
-//i = 0 - lav en variable og giv den værdien 0
-// i < animals.length - hvis i er mindre end længden af arreyet animals
-// i++ - hvis i er mindre end animals, gør i større
-//og den vil køre document.getelementbyid linje hvergang i ikke er større end animals
-// += tilføjer istedet for at erstatte
+//opret en funktion som kan beregne og retunere momsen af et beløb
+//anvend funktionen
 
+//Der behøves ikke være nogen parametre
+//Begynder man at skrive de samme ting flere gange, bør man overveje en function, GENBRUG koden
+
+//lærer eksempel
